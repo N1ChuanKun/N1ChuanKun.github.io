ed36c24c764c315db7ba4d7767ac962fe2983ce1
N1ChuanKun <nck1072971698@gmail.com>
2023-01-19 22:50:05 +0900

    Site updated: 2023-01-19 22:50:05

--- js/main.js
+++ js/main.js
@@ -1,65 +1,158 @@
-/* global REDEFINE */
+(() => {
+    var navEl = document.getElementById('theme-nav');
+    navEl.addEventListener('click', (e) => {
+        if (window.innerWidth <= 600) {
+            if (navEl.classList.contains('open')) {
+                navEl.style.height = ''
+            } else {
+                navEl.style.height = 48 + document.querySelector('#theme-nav .nav-items').clientHeight + 'px'
+            }
+            navEl.classList.toggle('open')
+        } else {
+            if (navEl.classList.contains('open')) {
+                navEl.style.height = ''
+                navEl.classList.remove('open')
+            }
+        }
+    })
+    window.addEventListener('resize', (e) => {
+        if (navEl.classList.contains('open')) {
+            navEl.style.height = 48 + document.querySelector('#theme-nav .nav-items').clientHeight + 'px'
+        }
+        if (window.innerWidth > 600) {
+            if (navEl.classList.contains('open')) {
+                navEl.style.height = ''
+                navEl.classList.remove('open')
+            }
+        }
+    })
 
-window.addEventListener('DOMContentLoaded', () => {
-
-  REDEFINE.themeInfo = {
-    theme: `Redefine v${REDEFINE.theme_config.version}`,
-    author: 'EvanNotFound',
-    repository: 'https://github.com/EvanNotFound/hexo-theme-redefine'
-  }
-
-  REDEFINE.localStorageKey = 'REDEFINE-THEME-STATUS';
-
-  REDEFINE.styleStatus = {
-    isExpandPageWidth: false,
-    isDark: false,
-    fontSizeLevel: 0,
-    isOpenPageAside: true
-  }
-
-  // print theme base info
-  REDEFINE.printThemeInfo = () => {
-    console.log(`\n %c ${REDEFINE.themeInfo.theme} %c ${REDEFINE.themeInfo.repository} \n`, `color: #fadfa3; background: #333; padding: 5px 0;`, `background: #fadfa3; padding: 5px 0;`);
-  }
-
-  // set styleStatus to localStorage
-  REDEFINE.setStyleStatus = () => {
-    localStorage.setItem(REDEFINE.localStorageKey, JSON.stringify(REDEFINE.styleStatus));
-  }
-
-  // get styleStatus from localStorage
-  REDEFINE.getStyleStatus = () => {
-    let temp = localStorage.getItem(REDEFINE.localStorageKey);
-    if (temp) {
-      temp = JSON.parse(temp);
-      for (let key in REDEFINE.styleStatus) {
-        REDEFINE.styleStatus[key] = temp[key];
-      }
-      return temp;
-    } else {
-      return null;
-    }
-  }
-
-  REDEFINE.refresh = () => {
-    REDEFINE.initUtils();
-    REDEFINE.initHeaderShrink();
-    REDEFINE.initModeToggle();
-    REDEFINE.initBack2Top();
-
-    if (REDEFINE.theme_config.local_search.enable === true) {
-      REDEFINE.initLocalSearch();
+    // a simple solution for managing cookies
+    const Cookies = new class {
+        get(key, fallback) {
+            const temp = document.cookie.split('; ').find(row => row.startsWith(key + '='))
+            if (temp) {
+                return temp.split('=')[1];
+            } else {
+                return fallback
+            }
+        }
+        set(key, value) {
+            document.cookie = key + '=' + value + '; path=' + document.body.getAttribute('data-config-root')
+        }
     }
 
-    if (REDEFINE.theme_config.code_block.copy === true) {
-      REDEFINE.initCodeCopy();
+    const ColorScheme = new class {
+        constructor() {
+            window.matchMedia('(prefers-color-scheme: dark)').addEventListener('change', () => { this.updateCurrent(Cookies.get('color-scheme', 'auto')) })
+        }
+        get() {
+            const stored = Cookies.get('color-scheme', 'auto')
+            this.updateCurrent(stored)
+            return stored
+        }
+        set(value) {
+            bodyEl.setAttribute('data-color-scheme', value)
+            Cookies.set('color-scheme', value)
+            this.updateCurrent(value)
+            return value
+        }
+        updateCurrent(value) {
+            var current = 'light'
+            if (value == 'auto') {
+                if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+                    current = 'dark'
+                }
+            } else {
+                current = value
+            }
+            document.body.setAttribute('data-current-color-scheme', current)
+        }
     }
 
-    if (REDEFINE.theme_config.lazyload.enable === true) {
-      REDEFINE.initLazyLoad();
-    }
-  }
+    if (document.getElementById('theme-color-scheme-toggle')) {
+        var bodyEl = document.body
+        var themeColorSchemeToggleEl = document.getElementById('theme-color-scheme-toggle')
+        var options = themeColorSchemeToggleEl.getElementsByTagName('input')
 
-  REDEFINE.printThemeInfo();
-  REDEFINE.refresh();
-});
+        if (ColorScheme.get()) {
+            bodyEl.setAttribute('data-color-scheme', ColorScheme.get())
+        }
+
+        for (const option of options) {
+            if (option.value == bodyEl.getAttribute('data-color-scheme')) {
+                option.checked = true
+            }
+            option.addEventListener('change', (ev) => {
+                var value = ev.target.value
+                ColorScheme.set(value)
+                for (const o of options) {
+                    if (o.value != value) {
+                        o.checked = false
+                    }
+                }
+            })
+        }
+    }
+
+    if (document.body.attributes['data-rainbow-banner']) {
+        var shown = false
+        switch (document.body.attributes['data-rainbow-banner-shown'].value) {
+            case 'always':
+                shown = true
+                break;
+            case 'auto':
+                shown = new Date().getMonth() + 1 == parseInt(document.body.attributes['data-rainbow-banner-month'].value, 10)
+                break;
+            default:
+                break;
+        }
+        if (shown) {
+            var banner = document.createElement('div')
+
+            banner.style.setProperty('--gradient', `linear-gradient(90deg, ${document.body.attributes['data-rainbow-banner-colors'].value})`)
+            banner.classList.add('rainbow-banner')
+
+            navEl.after(banner)
+        }
+    }
+
+    if (document.body.attributes['data-toc']) {
+        const content = document.getElementsByClassName('content')[0]
+        const maxDepth = document.body.attributes['data-toc-max-depth'].value
+
+        var headingSelector = ''
+        for (var i = 1; i <= maxDepth; i++) {
+            headingSelector += 'h' + i + ','
+        }
+        headingSelector = headingSelector.slice(0, -1)
+        const headings = content.querySelectorAll(headingSelector)
+
+        var source = []
+        headings.forEach((heading) => {
+            source.push({
+                html: heading.innerHTML,
+                href: heading.getElementsByClassName('headerlink')[0].attributes['href'].value
+            })
+        })
+
+        const toc = document.createElement('div')
+        toc.classList.add('toc')
+        for (const i in source) {
+            const item = document.createElement('p')
+            const link = document.createElement('a')
+            link.href = source[i].href
+            link.innerHTML = source[i].html
+            link.removeChild(link.getElementsByClassName('headerlink')[0])
+            item.appendChild(link)
+            toc.appendChild(item)
+        }
+
+        if (toc.children.length != 0) {
+            document.getElementsByClassName('post')[0].getElementsByClassName('divider')[0].after(toc)
+            const divider = document.createElement('div')
+            divider.classList.add('divider')
+            toc.after(divider)
+        }
+    }
+})()
